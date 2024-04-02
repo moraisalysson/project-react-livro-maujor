00b845ffdd2ed0689a905171d7ffc9782ca9575b
moraisalysson <amoraiswk@gmail.com>
2024-04-01 21:43:44 -0300

    excluindo arquivos desnecessários

--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home, contact
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('contato/', contact, name='contact'),
-]
--- courses/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, details
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('<slug>/', details, name='details'),
-]
